bed4d00a3e0dbd967908dd71cc5508406ef0e246
Fschmatz <fschmatz@hotmail.com>
2021-03-21 17:28:09 -0300

    v0.0.6

--- src/main/resources/db/changelog/migrations/001_criador_db.sql
+++ src/main/resources/db/changelog/migrations/001_criador_db.sql
@@ -49,30 +49,30 @@ INSERT INTO usuario(nome,login,senha) VALUES ('admin','admin','123456');
 --roolback DELETE FROM usuario WHERE id_usuario=id_usuario;
 
 --changeset fernando:6
-INSERT INTO categoria(id_categoria,descricao) values(1,'Alimentação');
+INSERT INTO categoria(descricao) values('Alimentação');
 --roolback DELETE FROM categoria WHERE id_categoria=1;
 
 --changeset fernando:7
-INSERT INTO categoria(id_categoria,descricao) values(2,'Gasolina');
+INSERT INTO categoria(descricao) values('Gasolina');
 --roolback DELETE FROM categoria WHERE id_categoria=2;
 
 --changeset fernando:8
-INSERT INTO categoria(id_categoria,descricao) values(3,'Criação de Chinchilas');
+INSERT INTO categoria(descricao) values('Criação de Chinchilas');
 --roolback DELETE FROM categoria WHERE id_categoria=3;
 
 --changeset fernando:9
-INSERT INTO categoria(id_categoria,descricao) values(4,'Melindres e Trambiques');
+INSERT INTO categoria(descricao) values('Melindres e Trambiques');
 --roolback DELETE FROM categoria WHERE id_categoria=3;
 
 
 --changeset fernando:10
-INSERT INTO receita(id_receita,nome,valor,id_categoria) values(1,'Vendas de Peles de Chinchila',5000.00,4);
+INSERT INTO receita(nome,valor,id_categoria) values('Vendas de Peles de Chinchila',5000.00,4);
 --roolback DELETE FROM categoria WHERE id_categoria=3;
 
 --changeset fernando:11
-INSERT INTO despesa(id_despesa,nome,valor,id_categoria) values(1,'Sushi',100.00,1);
+INSERT INTO despesa(nome,valor,id_categoria) values('Sushi',100.00,1);
 --roolback DELETE FROM categoria WHERE id_categoria=3;
 
 --changeset fernando:12
-INSERT INTO despesa(id_despesa,nome,valor,id_categoria) values(2,'Ração',1000.00,3);
+INSERT INTO despesa(nome,valor,id_categoria) values('Ração',1000.00,3);
 --roolback DELETE FROM categoria WHERE id_categoria=3;
